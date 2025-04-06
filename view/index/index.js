55de855ac56080818baf465519d8e99b8198eed3
PEN <uemasaki20011222@gmail.com>
2025-04-06 19:18:47 +0900

    20250406

--- view/index/index.js
+++ view/index/index.js
@@ -35,4 +35,66 @@ class Search{
     }
 }
 
+class MermaidUML{
+    constructor (umlPlace, umlSource, dl, umlName){
+        this.uml    = document.getElementById(umlPlace);
+        this.src    = document.getElementById(umlSource);
+        this.dl     = document.getElementById(dl);
+        this.name   = umlName;
+        this.src.addEventListener(
+            'keydown', 
+            this.key_press.bind(this)
+        );
+        mermaid.initialize(
+            {
+                startOnLoad     : true,
+            }
+        );
+        this.draw();
+        this.dl.addEventListener(
+            'click', 
+            () => {
+                this.download_svg();
+            }
+            
+        );
+    }
+    draw(){
+        this.uml.removeAttribute('data-processed');
+        this.uml.textContent = this.src.value;
+        mermaid.init();
+    }
+
+    key_press(pressKey){
+        if (pressKey.key === "Enter") {
+            this.draw()
+        }
+    }
+    download_svg() {
+        // svg要素を取得
+        let svgNode = this.uml.querySelector("svg");
+        let svgText = new XMLSerializer().serializeToString(svgNode);
+        let svgBlob = new Blob(
+            [svgText], 
+            { 
+                type: 'image/svg+xml;charset=utf-8' 
+            }
+        );
+        let svgUrl = URL.createObjectURL(svgBlob);
+      
+        // a要素を作ってダウンロード
+        let a = document.createElement('a');
+        a.href = svgUrl;
+        a.download = `${this.name}.svg`;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(svgUrl);
+    }
+      
+}
+
 new Search(model.bookmarkData);
+new MermaidUML("drawUML", "sourceUML", "umlDownload", "uml");
+
+
