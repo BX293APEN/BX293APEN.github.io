8ddba5975f485a0fceb7006599df8fcaa81bf10d
PEN <uemasaki20011222@gmail.com>
2025-05-10 15:50:35 +0900

    20250510

--- view/disp.js
+++ view/disp.js
@@ -13,3 +13,13 @@ function element_disp(dispIDList=[], deleteIDList=[],time = 300){
         );
     }
 }
+
+
+window.addEventListener( //デフォルト状態を指定
+    "load", 
+    element_disp(
+        [],
+        ["githubTip", "vimTip", "rn4020Tip", "linuxTip", "pythonTip", "visualstudioTip"],
+        250
+    )
+);
