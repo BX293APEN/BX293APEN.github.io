233ceff0247c4d7ea0319a199d6cede4e9cba7d3
PEN <uemasaki20011222@gmail.com>
2025-05-14 23:12:34 +0900

    ver.2.0.2

---
+++ model/tips/tips.js
@@ -0,0 +1,59 @@
+class TipsModel {
+    constructor(){
+        this.tipsData = {
+            "githubTip" : {
+                "href"      : "/BX293APEN/github.html",
+                "imgSrc"    : "/img/Github-Dark.svg", 
+                "title"     : "GitHub",
+                "text"      : "&emsp;Summary: How to use GitHub",
+                "svg"       : true
+                
+            },
+            "vimTip" : {
+                "href"      : "/BX293APEN/vim.html",
+                "imgSrc"    : "/img/VIM-Dark.svg", 
+                "title"     : "Vim",
+                "text"      : "&emsp;Want to learn Vim? <br>&emsp;This guide will help you understand.",
+                "svg"       : true
+
+            },
+            "rn4020Tip" : {
+                "href"      : "/html/tips/RN4020.html",
+                "imgSrc"    : "/img/RN4020_design.jpg", 
+                "title"     : "RN4020の簡易マニュアル",
+                "text"      : "&emsp;RN4020は、マイクロチップ社によって開発されたBluetooth Low Energy(BLE)通信モジュールです",
+                "svg"       : false
+
+            },
+            "linuxTip" : {
+                "href"      : "/html/tips/linux.html",
+                "imgSrc"    : "/img/Ubuntu-Dark.svg", 
+                "title"     : "Debian系Linuxセットアップガイド",
+                "text"      : "&emsp;Linuxを利用する上で、環境構築は非常に重要です",
+                "svg"       : true
+
+            },
+            "pythonTip" : {
+                "href"      : "/html/tips/python.html",
+                "imgSrc"    : "/img/Python-Dark.svg", 
+                "title"     : "Pythonの環境構築",
+                "text"      : "&emsp;Pythonをセットアップし、より快適なプログラミング環境を試してみましょう！",
+                "svg"       : true
+
+            },
+            "visualstudioTip" : {
+                "href"      : "/html/tips/visualstudio.html",
+                "imgSrc"    : "/img/VisualStudio-Dark.svg", 
+                "title"     : "Visual Studioの利用方法",
+                "text"      : "&emsp;フレームワークを利用することにより、大規模なソースコードの管理を容易にしましょう！",
+                "svg"       : true
+
+            },
+
+        }
+        this.tipsIndex = Object.keys(this.tipsData);
+        this.tipsIndex.sort();
+    }
+}
+
+tipsModel = new TipsModel();
---
+++ view/tips/tips.js
@@ -0,0 +1,125 @@
+class TipsView{
+    constructor(model, html, id, dispElement = []){
+        let insertPosition                  = document.getElementById(id);
+        if(html == "index"){
+            for(let tipID of model.tipsIndex){
+                let colTag                  = document.createElement("div");
+                colTag.classList.add("col");
+                colTag.id                   = tipID;
+
+                let aTag                    = document.createElement("a");
+                aTag.classList.add("link-body-emphasis", "text-decoration-none");
+                aTag.href                   = model.tipsData[tipID]["href"];
+                
+                let divCoverTag             = document.createElement("div");
+                divCoverTag.classList.add("card", "shadow-sm", "h-100");
+
+                let imgTag                  = document.createElement("img");
+                imgTag.src                  = model.tipsData[tipID]["imgSrc"];
+                imgTag.classList.add("bd-placeholder-img");
+                imgTag.ariaHidden           = "true";
+
+                if (model.tipsData[tipID]["svg"]){
+                    imgTag.width            = "100%";
+                    imgTag.height           = 150;
+                    imgTag.classList.add("card-img-top");
+                }
+                else{
+                    imgTag.width            = 120;
+                    imgTag.classList.add("mx-auto");
+                }
+
+                divCoverTag.appendChild(imgTag);
+
+                let divTag                  = document.createElement("div");
+                divTag.classList.add("card-body");
+
+                let h6Tag                   = document.createElement("h6");
+                h6Tag.innerHTML             = model.tipsData[tipID]["title"];
+                h6Tag.classList.add("mb-0", "fw-bold");
+                divTag.appendChild(h6Tag);
+
+                let pTag                    = document.createElement("p");
+                pTag.innerHTML              = model.tipsData[tipID]["text"];
+                pTag.classList.add("card-text");
+                divTag.appendChild(pTag);
+
+                divCoverTag.appendChild(divTag);
+                aTag.appendChild(divCoverTag);
+                colTag.appendChild(aTag);
+                insertPosition.appendChild(colTag);
+            }
+        }
+
+        else{
+            let coverTag                = document.createElement("div");
+            coverTag.classList.add("p-2");
+            let titleTag                = document.createElement("h4");
+            titleTag.classList.add("fst-italic");
+            titleTag.innerHTML          = "Tips";
+            coverTag.appendChild(titleTag);
+
+            let ulTag                   = document.createElement("ul");
+            ulTag.classList.add("list-unstyled");
+
+            for(let tipID of model.tipsIndex){
+                if(dispElement.includes(tipID)){
+                    let liTag                   = document.createElement("li");
+                    liTag.id                    = tipID;
+
+                    let aTag                    = document.createElement("a");
+                    aTag.classList.add(
+                        "d-flex", 
+                        "flex-column", 
+                        "flex-lg-row", 
+                        "gap-3", 
+                        "align-items-start", 
+                        "align-items-lg-center", 
+                        "py-3", 
+                        "link-body-emphasis", 
+                        "text-decoration-none", 
+                        "border-top" 
+                    );
+
+                    aTag.href                   = model.tipsData[tipID]["href"];
+
+                    let imgTag                  = document.createElement("img");
+                    imgTag.src                  = model.tipsData[tipID]["imgSrc"]; // src="https://skillicons.dev/icons?i=github"
+                    imgTag.classList.add("bd-placeholder-img");
+                    imgTag.ariaHidden           = "true";
+
+                    if (model.tipsData[tipID]["svg"]){
+                        imgTag.width            = 100;
+                        imgTag.height           = 96;
+                    }
+                    else{
+                        imgTag.width            = 100;
+                        imgTag.classList.add("mx-auto");
+                    }
+                    aTag.appendChild(imgTag);
+
+                    let divTag                  = document.createElement("div");
+                    divTag.classList.add("col-lg-8");
+
+                    let h6Tag                   = document.createElement("h6");
+                    h6Tag.innerHTML             = model.tipsData[tipID]["title"];
+                    h6Tag.classList.add("mb-0", "fw-bold");
+                    divTag.appendChild(h6Tag);
+
+                    let smallTag                    = document.createElement("small");
+                    smallTag.innerHTML              = model.tipsData[tipID]["text"];
+                    smallTag.classList.add("text-body-secondary");
+                    divTag.appendChild(smallTag);
+
+                    aTag.appendChild(divTag);
+                    liTag.appendChild(aTag);
+                    ulTag.appendChild(liTag);
+                }
+                
+            }
+            coverTag.appendChild(ulTag);
+            insertPosition.appendChild(coverTag);
+        }
+        
+    }
+}
