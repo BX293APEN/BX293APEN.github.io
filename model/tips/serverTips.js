bc7030dc5d818abc766fd0b8b336f521a4041f5f
PEN <uemasaki20011222@gmail.com>
2025-06-24 08:16:30 +0900

    20250624

---
+++ model/tips/serverTips.js
@@ -0,0 +1,20 @@
+class ServerModel{
+    constructor(){
+        this.page =[
+            {
+                "page" : 1,
+                "href" : "/html/tips/server.html"
+            },
+            {
+                "page" : 2,
+                "href" : "/html/tips/server/dns.html"
+            },
+            {
+                "page" : 3,
+                "href" : "/html/tips/server/ftp.html"
+            }
+        ]
+    }
+}
+
+let serverModel = new ServerModel();
