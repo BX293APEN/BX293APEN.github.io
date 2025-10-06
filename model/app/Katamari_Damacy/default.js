f7def652d6cf386a70f0802ff4d3ff2b453b6678
PEN <uemasaki20011222@gmail.com>
2025-10-06 22:34:05 +0900

    20251006

--- model/app/Katamari_Damacy/default.js
+++ model/app/Katamari_Damacy/default.js
@@ -21,7 +21,7 @@ class KatamariDamacyModel{
                 "timeLimit"             : 600,          // 秒数
                 "targetCount"           : 150,
                 "sizeMultiplier"        : 1.15,
-                "growthBonus"           : 1.2,
+                "growthBonus"           : 1.3,
                 "objects": {                            // オブジェクトの数を指定
                     "tiny"              : 80,           // 極小
                     "small"             : 60,           // 小
@@ -36,10 +36,10 @@ class KatamariDamacyModel{
                 "timeLimit"             : 480,
                 "targetCount"           : 200,
                 "sizeMultiplier"        : 1.3,
-                "growthBonus"           : 1.0,
+                "growthBonus"           : 1.2,
                 "objects": {                            // オブジェクトの数を指定
                     "tiny"              : 80,           // 極小
-                    "small"             : 60,           // 小
+                    "small"             : 70,           // 小
                     "medium"            : 40,           // 中
                     "large"             : 25,           // 大
                     "huge"              : 15,           // 超大
@@ -51,13 +51,13 @@ class KatamariDamacyModel{
                 "timeLimit"             : 360,
                 "targetCount"           : 250,
                 "sizeMultiplier"        : 1.5,
-                "growthBonus"           : 0.8,
+                "growthBonus"           : 1.0,
                 "objects": {                            // オブジェクトの数を指定
                     "tiny"              : 80,           // 極小
                     "small"             : 60,           // 小
-                    "medium"            : 40,           // 中
-                    "large"             : 25,           // 大
-                    "huge"              : 15,           // 超大
+                    "medium"            : 50,           // 中
+                    "large"             : 30,           // 大
+                    "huge"              : 25,           // 超大
                     "moving"            : 10            // 動く
                 }
             }
