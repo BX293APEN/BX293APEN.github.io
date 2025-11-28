c432367c1eff1e16ad664cff608610134891a30b
PEN <uemasaki20011222@gmail.com>
2025-11-28 21:21:09 +0900

    20251128

---
+++ model/env.js
@@ -0,0 +1,14 @@
+class IOSet{
+    constructor(){
+        this.imgPath    = "/img"
+        this.configPath = "/config"
+        this.cssPath    = "/css"
+        this.htmlPath   = "/html"
+
+        this.model      = "/model"
+        this.template   = "/template"
+        this.view       = "/view"
+    }
+}
+
+const ioset = new IOSet();
