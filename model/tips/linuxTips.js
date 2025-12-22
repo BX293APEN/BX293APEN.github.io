548b10f43010c418f41e81f4ead02d11ec634a2e
PEN <uemasaki20011222@gmail.com>
2025-12-22 10:40:50 +0900

    network

--- model/tips/linuxTips.js
+++ model/tips/linuxTips.js
@@ -7,20 +7,24 @@ class LinuxModel{
             },
             {
                 "page" : 2,
-                "href" : "/html/tips/linux/raspi.html"
+                "href" : "/html/tips/linux/network.html"
             },
             {
                 "page" : 3,
-                "href" : "/html/tips/linux/daemon.html"
+                "href" : "/html/tips/linux/config.html"
             },
             {
                 "page" : 4,
-                "href" : "/html/tips/linux/format.html"
+                "href" : "/html/tips/linux/raspi.html"
             },
             {
                 "page" : 5,
-                "href" : "/html/tips/linux/config.html"
-            }
+                "href" : "/html/tips/linux/daemon.html"
+            },
+            {
+                "page" : 6,
+                "href" : "/html/tips/linux/format.html"
+            },
         ]
     }
 }
