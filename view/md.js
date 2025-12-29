274b21b20d9c84ed0b20a9d1a86d688dfcf628ef
PEN <uemasaki20011222@gmail.com>
2025-12-29 11:44:03 +0900

    table

--- view/md.js
+++ view/md.js
@@ -1,39 +1,19 @@
-class MarkDownLoader {
+class MarkDownParent {
     constructor(
-        id              = "md-text",
-        mdFile          = "/DLC/TA%E6%83%85%E5%A0%B1%E6%BC%94%E7%BF%92.md", 
-        loadJS          = ["/view/highlight.min.js", "/view/marked.min.js"]
+        id              = "md-text"
     ) {
-        this.id         = id
-        this.mdFile     = mdFile
-        this.loadJS     = loadJS
-        // constructor の this に固定したい場合はアロー関数が必須 
-        Promise.all(
-            this.loadJS.map(src => this.jsLoad(src))
-        ).then(
-            () => { 
-                return fetch(this.mdFile); 
-            }
-        ).then(
-            response => response.text()
-        ).then(
-            data => { 
-                const mdHTML                                = marked.parse(data); 
-                document.getElementById(this.id).innerHTML  = mdHTML; 
-                for (const element of md.querySelectorAll("table")) { 
-                    element.classList.add("table", "table-bordered"); 
-                } 
-                for (const element of md.querySelectorAll("th")) { 
-                    element.classList.add("text-nowrap"); 
-                } 
-                hljs.highlightAll(); 
-            }
-        ).catch(
-            error => console.error(
-                "読み込みに失敗:", 
-                error
-            )
-        );
+        this.id         = id;
+    }
+
+    decorate() {
+        const md = document.getElementById(this.id);
+        for (const element of md.querySelectorAll("table")) {
+            element.classList.add("table", "table-bordered");
+        }
+        for (const element of md.querySelectorAll("th")) {
+            element.classList.add("text-nowrap");
+        }
+        hljs.highlightAll();
     }
 
     jsLoad(src) {
@@ -49,12 +29,50 @@ class MarkDownLoader {
     }
 }
 
-class MarkDownMaker {
+
+
+class MarkDownLoader extends MarkDownParent {
+    constructor(
+        id              = "md-text",
+        mdFile          = "/DLC/TA%E6%83%85%E5%A0%B1%E6%BC%94%E7%BF%92.md", 
+        loadJS          = ["/view/highlight.min.js", "/view/marked.min.js"]
+    ) {
+        super(id);      // 最初に親コンストラクタを実行
+
+        this.mdFile     = mdFile
+        this.loadJS     = loadJS
+
+        // constructor の this に固定したい場合はアロー関数が必須 
+        Promise.all(
+            this.loadJS.map(src => this.jsLoad(src))
+        ).then(
+            () => { 
+                return fetch(this.mdFile); 
+            }
+        ).then(
+            response => response.text()
+        ).then(
+            data => { 
+                const mdHTML                                = marked.parse(data); 
+                document.getElementById(this.id).innerHTML  = mdHTML; 
+                this.decorate();
+            }
+        ).catch(
+            error => console.error(
+                "読み込みに失敗:", 
+                error
+            )
+        );
+    }
+}
+
+class MarkDownMaker extends MarkDownParent {
     constructor(
         id              = "md-text",
         loadJS          = ["/view/highlight.min.js", "/view/marked.min.js"]
     ) {
-        this.id         = id
+        super(id);      // 最初に親コンストラクタを実行
+        
         this.loadJS     = loadJS
         // constructor の this に固定したい場合はアロー関数が必須 
         Promise.all(
@@ -64,32 +82,13 @@ class MarkDownMaker {
                 const md                    = document.getElementById(this.id);
                 const mdHTML                = marked.parse(md.innerText);
                 md.innerHTML                = mdHTML; 
-                for (const element of md.querySelectorAll("table")) { 
-                    element.classList.add("table", "table-bordered"); 
-                } 
-                for (const element of md.querySelectorAll("th")) { 
-                    element.classList.add("text-nowrap"); 
-                } 
-                hljs.highlightAll(); 
-
+                this.decorate();
             }
         ).catch(
             error => console.error(
                 "読み込みに失敗:", 
                 error
             )
-        );
-    }
-
-    jsLoad(src) {
-        return new Promise(
-            (resolve, reject) => {
-                const scriptTag     = document.createElement("script");
-                scriptTag.src       = src;
-                scriptTag.onload    = resolve;
-                scriptTag.onerror   = reject;
-                document.head.appendChild(scriptTag);
-            }
         );
     }
 }
