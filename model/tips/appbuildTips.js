99e601346052f8841dd1299b56912f846e303228
PEN <uemasaki20011222@gmail.com>
2026-07-26 20:19:23 +0900

    blender

---
+++ model/tips/appbuildTips.js
@@ -0,0 +1,16 @@
+class APPBuildModel{
+    constructor(){
+        this.page =[
+            {
+                "page" : 1,
+                "href" : "/html/tips/appbuild.html"
+            },
+            {
+                "page" : 2,
+                "href" : "/html/tips/appbuild/blender.html"
+            },
+        ]
+    }
+}
+
+let appBuildModel = new APPBuildModel();
