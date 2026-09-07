8a14668150e85c3cb129edb908c50d6614566622
PEN <uemasaki20011222@gmail.com>
2026-09-07 15:31:11 +0900

    laws

---
+++ model/about/securityTips.js
@@ -0,0 +1,16 @@
+class SecurityModel{
+    constructor(){
+        this.page =[
+            {
+                "page" : 1,
+                "href" : "/html/about/security/index.html"
+            },
+            {
+                "page" : 2,
+                "href" : "/html/about/security/security-laws.html"
+            },
+        ]
+    }
+}
+
+let securityModel = new SecurityModel();
